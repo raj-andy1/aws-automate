22c3d89fd21ef10c2b30d31b39a34fe6cabb6d0d
Andy Rajagopalan <ranandthirth@gmail.com>
2019-09-25 15:39:59 -0700

    reverting to older version, and create new file to use params

--- create_stack.py
+++ create_stack.py
@@ -1,46 +1,30 @@
 #sample python code to create multiple cloudformation stacks
-
-'''
-Instructions to create multiple stacks of Jira, Confluence or Bitbucket DC
-1. Clone the repository aws-automate
-
-
-8. Logon to the AWS console and check the stack creation process
-'''
-
 import boto3
 import random
-import yaml
-import sys
 
-#parameters to create number of stacks
 stack_list = []
-num_stack = 2
+num_stack = 61
+user_str = 'jira'
 salt_flag = False
-start_num = 1
+start_num = 2
 
 #boto params
 region_nm = 'eu-central-1'
 
-#stack parameters are loaded into a yaml file for easy readability and access
 
-'''
-stacks = {'jiradc11-latest-1':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml','jira_stack_latest_params.yaml'],
-		'jiradc22-latest-2':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml','jira_stack_latest_params.yaml'],
-		'jiradc33-er-1':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml','jira_stack_er_params.yaml'],
-		'jiradc44-er-2':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml','jira_stack_er_params.yaml'],
-		'cnfdc11-latest-1':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-confluence/templates/quickstart-confluence-master.template.yaml','confluence_stack_latest_params.yaml'],
-		'cnfdc22-latest-2':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-confluence/templates/quickstart-confluence-master.template.yaml','confluence_stack_latest_params.yaml'],
-		'cnfdc33-er-3':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-confluence/templates/quickstart-confluence-master.template.yaml','confluence_stack_er_params.yaml'],
-		'cnfdc44-er-4':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-confluence/templates/quickstart-confluence-master.template.yaml','confluence_stack_er_params.yaml']}
-'''
-stacks = {
-		
-		'bbdc-2':['https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-bitbucket/templates/quickstart-bitbucket-dc.template.yaml','bitbucket_stack_params.yaml'],
-}
+for num in range(start_num,(num_stack+1)):
+	if salt_flag: # if flag is present, add the salt to the instance name string
+		salt = random.randrange(10,500,3)
+		stack_name = user_str + '-' + str(salt) + '-'
+	else:
+		stack_name = user_str + '-'
+	stack_name = stack_name + str(num).zfill(3)
+	stack_list.append(stack_name)
+
+print ('List of stacks to be created:',stack_list)
+
 
 client = boto3.client('cloudformation',region_name=region_nm)
-
 
 while True:
 	response = input('Press "P" to proceed or "C" to cancel:')
@@ -49,75 +33,72 @@ while True:
 		break
 	elif response == 'P' or response == 'p':
 		print ('Creating Instances')
-		for k,v in stacks.items():
-			stack = k
-			temp_url, params_file = v
-			stack_params = yaml.safe_load(open(params_file))
-			#print ('stack_name:', stack)
-			#print ('temp_url:', temp_url)
-			#print ('stack_params:', stack_params)
-			try: 
-				response = client.create_stack(
-					StackName = stack,
-					TemplateURL = temp_url,
-					Parameters = stack_params,
-    				OnFailure='DO_NOTHING',
-    		 		Capabilities=['CAPABILITY_IAM',],
-    				Tags=[
-    				{'Key': 'resource_owner','Value': 'arajagopalan'},
-    				{'Key': 'service_name','Value': 'open-vienna-2019'},
-    				{'Key': 'business_unit','Value': 'FieldOps'},
-    				{'Key':'Name','Value':stack},
-    				],
-    				ClientRequestToken=str(random.randrange(10,1000,3)),
-    				EnableTerminationProtection=False,
-    				)
-				if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-					print('Created Stack: %s' % stack)
-				else:
-					print (response)
-			except Exception as e:
-				print (e)
+		for stack in stack_list:
+			response = client.create_stack(
+				StackName = stack,
+				TemplateURL='https://open-hol-dc-on-aws.s3.eu-central-1.amazonaws.com/templates/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml',
+				Parameters= [
+				{'ParameterKey':'JiraProduct','ParameterValue':'Software','UsePreviousValue':False},
+				{'ParameterKey':'JiraVersion','ParameterValue':'7.13.5','UsePreviousValue':False},
+				{'ParameterKey':'ClusterNodeInstanceType','ParameterValue':'c5.xlarge','UsePreviousValue':False},
+				{'ParameterKey':'ClusterNodeMax','ParameterValue':'1','UsePreviousValue':False},
+				{'ParameterKey':'ClusterNodeMin','ParameterValue':'1','UsePreviousValue':False},
+				{'ParameterKey':'ClusterNodeVolumeSize','ParameterValue':'50','UsePreviousValue':False},
+				{'ParameterKey':'DBInstanceClass','ParameterValue':'db.m4.large','UsePreviousValue':False},
+				{'ParameterKey':'DBIops','ParameterValue':'1000','UsePreviousValue':False},
+				{'ParameterKey':'DBMasterUserPassword','ParameterValue':'Charlie101','UsePreviousValue':False},
+				{'ParameterKey':'DBMultiAZ','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'DBPassword','ParameterValue':'Charlie101','UsePreviousValue':False},
+				{'ParameterKey':'DBStorage','ParameterValue':'200','UsePreviousValue':False},
+				{'ParameterKey':'DBStorageEncrypted','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'DBStorageType','ParameterValue':'General Purpose (SSD)','UsePreviousValue':False},
+				{'ParameterKey':'InternetFacingLoadBalancer','ParameterValue':'true','UsePreviousValue':False},
+				{'ParameterKey':'CidrBlock','ParameterValue':'0.0.0.0/0','UsePreviousValue':False},
+				{'ParameterKey':'KeyPairName','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'SSLCertificateARN','ParameterValue':'arn:aws:acm:eu-central-1:755021131661:certificate/424eb0f8-24bd-4a1c-96db-028eccab1bf9','UsePreviousValue':False},
+				{'ParameterKey':'CustomDnsName','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'HostedZone','ParameterValue':'open.atlassian.guru.','UsePreviousValue':False},
+				{'ParameterKey':'DeploymentAutomationRepository','ParameterValue':'https://bitbucket.org/atlassian/dc-deployments-automation.git','UsePreviousValue':False},
+				{'ParameterKey':'DeploymentAutomationBranch','ParameterValue':'master','UsePreviousValue':False},
+				{'ParameterKey':'DeploymentAutomationPlaybook','ParameterValue':'aws_jira_dc_node.yml','UsePreviousValue':False},
+				{'ParameterKey':'DeploymentAutomationKeyName','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'TomcatContextPath','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'CatalinaOpts','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'JvmHeapOverride','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'DBPoolMaxSize','ParameterValue':'20','UsePreviousValue':False},
+				{'ParameterKey':'DBPoolMinSize','ParameterValue':'20','UsePreviousValue':False},
+				{'ParameterKey':'DBMaxIdle','ParameterValue':'20','UsePreviousValue':False},
+				{'ParameterKey':'DBMaxWaitMillis','ParameterValue':'10000','UsePreviousValue':False},
+				{'ParameterKey':'DBMinEvictableIdleTimeMillis','ParameterValue':'18000','UsePreviousValue':False},
+				{'ParameterKey':'DBMinIdle','ParameterValue':'10','UsePreviousValue':False},
+				{'ParameterKey':'DBRemoveAbandoned','ParameterValue':'true','UsePreviousValue':False},
+				{'ParameterKey':'DBRemoveAbandonedTimeout','ParameterValue':'60','UsePreviousValue':False},
+				{'ParameterKey':'DBTestOnBorrow','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'DBTestWhileIdle','ParameterValue':'true','UsePreviousValue':False},
+				{'ParameterKey':'DBTimeBetweenEvictionRunsMillis','ParameterValue':'60000','UsePreviousValue':False},
+				{'ParameterKey':'MailEnabled','ParameterValue':'true','UsePreviousValue':False},
+				{'ParameterKey':'TomcatAcceptCount','ParameterValue':'10','UsePreviousValue':False},
+				{'ParameterKey':'TomcatConnectionTimeout','ParameterValue':'20000','UsePreviousValue':False},
+				{'ParameterKey':'TomcatDefaultConnectorPort','ParameterValue':'8080','UsePreviousValue':False},
+				{'ParameterKey':'TomcatEnableLookups','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'TomcatMaxThreads','ParameterValue':'200','UsePreviousValue':False},
+				{'ParameterKey':'TomcatMinSpareThreads','ParameterValue':'10','UsePreviousValue':False},
+				{'ParameterKey':'TomcatProtocol','ParameterValue':'HTTP/1.1','UsePreviousValue':False},
+				{'ParameterKey':'TomcatRedirectPort','ParameterValue':'8443','UsePreviousValue':False},
+				{'ParameterKey':'TomcatScheme','ParameterValue':'https','UsePreviousValue':False},
+				],
+    			OnFailure='DO_NOTHING',
+    		 	Capabilities=['CAPABILITY_IAM',],
+    			Tags=[
+    			{'Key': 'resource_owner','Value': 'andyr'},
+    			{'Key': 'Purpose','Value': 'jira-testing'},
+    			{'Key': 'business_unit','Value': 'FieldOps'},
+    			{'Key':'Name','Value':stack},
+    			],
+    			ClientRequestToken=str(random.randrange(10,1000,3)),
+    			EnableTerminationProtection=False,
+    			)
+			if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+				print('Created Stack: %s' % stack)
+	#print (response)'''
 		exit()
-
-
-'''
-# old code to allow use of template type, parameter files...etc
-def get_template_url(template_type='jiradc'):
-	try:
-		template_type = sys.argv[1]
-	except IndexError:
-		template_url = 'https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml'
-		params_file = 'jira_stack_params.yaml'
-	if template_type == 'bbdc': #bitbucket qs s3 url
-		#template_url = 'https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-bitbucket/templates/quickstart-bitbucket-dc.template.yaml'
-		template_url = 'https://atl-andyr-cloudformation.s3.amazonaws.com/templates/quickstart-atlassian-bitbucket/templates/quickstart-bitbucket-dc.template.yaml'
-		params_file = 'bitbucket_stack_params.yaml'
-	elif template_type == 'cnfdc':  #confluence qs s3 url
-		template_url = 'https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-confluence/templates/quickstart-confluence-master.template.yaml'
-		params_file = 'confluence_stack_params.yaml'
-	params = yaml.safe_load(open(params_file))
-	
-	return (template_url, params, template_type)
-
-user_str = get_template_url()[2]
-
-for num in range(start_num,(start_num+num_stack)):
-	if salt_flag: # if flag is present, add the salt to the instance name string
-		salt = random.randrange(10,500,3)
-		stack_name = user_str + str(salt) + '-'
-	else:
-		stack_name = user_str + '-' + 'latest'
-	stack_name = stack_name + str(num).zfill(1)
-	stack_list.append(stack_name)
-
-
-stack_list = ['bbdc-1']
-print ('List of stacks to be created:',stack_list)
-
-temp_url = get_template_url()[0]
-stack_params = get_template_url()[1]
-
-#print (temp_url)
-#print (stack_params)
-'''
