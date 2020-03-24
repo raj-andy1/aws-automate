982d8ef06bc762769860ee3d0234c6ab2937fa66
Andy Rajagopalan <ranandthirth@gmail.com>
2020-03-24 06:36:50 -0700

    add comments and change var name to be consistent

--- update_hosted_zone.py
+++ update_hosted_zone.py
@@ -3,13 +3,14 @@
 import boto3
 import json
 
-hz_ns = {'user-020.charlieoil.com': ['ns-302.awsdns-37.com', 'ns-1350.awsdns-40.org', 'ns-1819.awsdns-35.co.uk', 'ns-720.awsdns-26.net']}
+#output of create_mul_hosted_zone.py with child domains and NS records
+hz_nm = {'user-020.charlieoil.com': ['ns-302.awsdns-37.com', 'ns-1350.awsdns-40.org', 'ns-1819.awsdns-35.co.uk', 'ns-720.awsdns-26.net']}
 
 client = boto3.client('route53')
 
-for hz in hz_ns:
+for hz in hz_nm:
     update_rr_response = client.change_resource_record_sets(
-        HostedZoneId='Z3LTN44WAE5CAU',
+        HostedZoneId='Z3LTN44WAE5CAU', #hosted zone id of the parent domain
 	   ChangeBatch={
 	   'Changes': [{
 	   'Action': 'CREATE',
@@ -18,9 +19,9 @@ for hz in hz_ns:
         'Type': 'NS',
         'TTL': 60,
         'ResourceRecords': [
-            {'Value': hz_ns[hz][0]},
-            {'Value': hz_ns[hz][1]},
-            {'Value': hz_ns[hz][2]},
-            {'Value': hz_ns[hz][3]},
+            {'Value': hz_nm[hz][0]},
+            {'Value': hz_nm[hz][1]},
+            {'Value': hz_nm[hz][2]},
+            {'Value': hz_nm[hz][3]},
          ],}},]})
     print (update_rr_response)
