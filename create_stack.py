719c8283ae210db07590324cb77b44ace22f197f
Maansi Chandira <mchandira@atlassian.com>
2020-09-30 15:09:13 -0700

    updated scripts with recent lab changes

--- create_stack.py
+++ create_stack.py
@@ -3,18 +3,18 @@ import boto3
 import random
 
 stack_list = []
-num_stack = 61
-user_str = 'jira'
+num_stack = 17
+user_str = 'jira-backup-west'
 salt_flag = False
 start_num = 2
 
 #boto params
-region_nm = 'eu-central-1'
+region_nm = 'us-west-2'
 
 
 for num in range(start_num,(num_stack+1)):
 	if salt_flag: # if flag is present, add the salt to the instance name string
-		salt = random.randrange(10,500,3)
+		salt = random.randrange(100,500,3)
 		stack_name = user_str + '-' + str(salt) + '-'
 	else:
 		stack_name = user_str + '-'
@@ -36,32 +36,37 @@ while True:
 		for stack in stack_list:
 			response = client.create_stack(
 				StackName = stack,
-				TemplateURL='https://open-hol-dc-on-aws.s3.eu-central-1.amazonaws.com/templates/quickstart-atlassian-jira/templates/quickstart-jira-dc.template.yaml',
+				TemplateURL='https://atl-events.s3.amazonaws.com/templates/jira-quickstart.yaml',
 				Parameters= [
 				{'ParameterKey':'JiraProduct','ParameterValue':'Software','UsePreviousValue':False},
-				{'ParameterKey':'JiraVersion','ParameterValue':'7.13.5','UsePreviousValue':False},
+				{'ParameterKey':'JiraVersion','ParameterValue':'8.12.1','UsePreviousValue':False},
+				{'ParameterKey':'CloudWatchIntegration','ParameterValue':'Metrics and Logs','UsePreviousValue':False},
 				{'ParameterKey':'ClusterNodeInstanceType','ParameterValue':'c5.xlarge','UsePreviousValue':False},
 				{'ParameterKey':'ClusterNodeMax','ParameterValue':'1','UsePreviousValue':False},
 				{'ParameterKey':'ClusterNodeMin','ParameterValue':'1','UsePreviousValue':False},
 				{'ParameterKey':'ClusterNodeVolumeSize','ParameterValue':'50','UsePreviousValue':False},
-				{'ParameterKey':'DBInstanceClass','ParameterValue':'db.m4.large','UsePreviousValue':False},
-				{'ParameterKey':'DBIops','ParameterValue':'1000','UsePreviousValue':False},
-				{'ParameterKey':'DBMasterUserPassword','ParameterValue':'Charlie101','UsePreviousValue':False},
-				{'ParameterKey':'DBMultiAZ','ParameterValue':'false','UsePreviousValue':False},
-				{'ParameterKey':'DBPassword','ParameterValue':'Charlie101','UsePreviousValue':False},
-				{'ParameterKey':'DBStorage','ParameterValue':'200','UsePreviousValue':False},
-				{'ParameterKey':'DBStorageEncrypted','ParameterValue':'false','UsePreviousValue':False},
-				{'ParameterKey':'DBStorageType','ParameterValue':'General Purpose (SSD)','UsePreviousValue':False},
-				{'ParameterKey':'InternetFacingLoadBalancer','ParameterValue':'true','UsePreviousValue':False},
-				{'ParameterKey':'CidrBlock','ParameterValue':'0.0.0.0/0','UsePreviousValue':False},
-				{'ParameterKey':'KeyPairName','ParameterValue':'','UsePreviousValue':False},
-				{'ParameterKey':'SSLCertificateARN','ParameterValue':'arn:aws:acm:eu-central-1:755021131661:certificate/424eb0f8-24bd-4a1c-96db-028eccab1bf9','UsePreviousValue':False},
-				{'ParameterKey':'CustomDnsName','ParameterValue':'','UsePreviousValue':False},
-				{'ParameterKey':'HostedZone','ParameterValue':'open.atlassian.guru.','UsePreviousValue':False},
 				{'ParameterKey':'DeploymentAutomationRepository','ParameterValue':'https://bitbucket.org/atlassian/dc-deployments-automation.git','UsePreviousValue':False},
 				{'ParameterKey':'DeploymentAutomationBranch','ParameterValue':'master','UsePreviousValue':False},
 				{'ParameterKey':'DeploymentAutomationPlaybook','ParameterValue':'aws_jira_dc_node.yml','UsePreviousValue':False},
+				{'ParameterKey':'DeploymentAutomationCustomParams','ParameterValue':'','UsePreviousValue':False},
 				{'ParameterKey':'DeploymentAutomationKeyName','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'DBEngine','ParameterValue':'PostgreSQL','UsePreviousValue':False},
+				{'ParameterKey':'DBEngineVersion','ParameterValue':'9','UsePreviousValue':False},
+				{'ParameterKey':'DBInstanceClass','ParameterValue':'db.m5.large','UsePreviousValue':False},
+				{'ParameterKey':'DBIops','ParameterValue':'1000','UsePreviousValue':False},
+				{'ParameterKey':'DBMasterUserPassword','ParameterValue':'Test#1234','UsePreviousValue':False},
+				{'ParameterKey':'DBMultiAZ','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'DBPassword','ParameterValue':'Test#1234','UsePreviousValue':False},
+				{'ParameterKey':'DBStorage','ParameterValue':'200','UsePreviousValue':False},
+				{'ParameterKey':'DBStorageEncrypted','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'DBStorageType','ParameterValue':'General Purpose (SSD)','UsePreviousValue':False},
+				{'ParameterKey':'BastionHostRequired','ParameterValue':'false','UsePreviousValue':False},
+				{'ParameterKey':'KeyPairName','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'InternetFacingLoadBalancer','ParameterValue':'true','UsePreviousValue':False},
+				{'ParameterKey':'CidrBlock','ParameterValue':'0.0.0.0/0','UsePreviousValue':False},
+				{'ParameterKey':'SSLCertificateARN','ParameterValue':'arn:aws:acm:us-west-2:948417468652:certificate/6a8ad93d-f2d4-49ae-a4d5-d4c7db8146f9','UsePreviousValue':False},
+				{'ParameterKey':'CustomDnsName','ParameterValue':'','UsePreviousValue':False},
+				{'ParameterKey':'HostedZone','ParameterValue':'pe.atlassian.camp.','UsePreviousValue':False},
 				{'ParameterKey':'TomcatContextPath','ParameterValue':'','UsePreviousValue':False},
 				{'ParameterKey':'CatalinaOpts','ParameterValue':'','UsePreviousValue':False},
 				{'ParameterKey':'JvmHeapOverride','ParameterValue':'','UsePreviousValue':False},
@@ -69,7 +74,7 @@ while True:
 				{'ParameterKey':'DBPoolMinSize','ParameterValue':'20','UsePreviousValue':False},
 				{'ParameterKey':'DBMaxIdle','ParameterValue':'20','UsePreviousValue':False},
 				{'ParameterKey':'DBMaxWaitMillis','ParameterValue':'10000','UsePreviousValue':False},
-				{'ParameterKey':'DBMinEvictableIdleTimeMillis','ParameterValue':'18000','UsePreviousValue':False},
+				{'ParameterKey':'DBMinEvictableIdleTimeMillis','ParameterValue':'180000','UsePreviousValue':False},
 				{'ParameterKey':'DBMinIdle','ParameterValue':'10','UsePreviousValue':False},
 				{'ParameterKey':'DBRemoveAbandoned','ParameterValue':'true','UsePreviousValue':False},
 				{'ParameterKey':'DBRemoveAbandonedTimeout','ParameterValue':'60','UsePreviousValue':False},
@@ -85,12 +90,14 @@ while True:
 				{'ParameterKey':'TomcatMinSpareThreads','ParameterValue':'10','UsePreviousValue':False},
 				{'ParameterKey':'TomcatProtocol','ParameterValue':'HTTP/1.1','UsePreviousValue':False},
 				{'ParameterKey':'TomcatRedirectPort','ParameterValue':'8443','UsePreviousValue':False},
-				{'ParameterKey':'TomcatScheme','ParameterValue':'https','UsePreviousValue':False},
+				{'ParameterKey':'QSS3BucketName','ParameterValue':'aws-quickstart','UsePreviousValue':False},
+				{'ParameterKey':'QSS3KeyPrefix','ParameterValue':'quickstart-atlassian-jira/','UsePreviousValue':False},
+				{'ParameterKey':'ExportPrefix','ParameterValue':'ATL-','UsePreviousValue':False},
 				],
     			OnFailure='DO_NOTHING',
     		 	Capabilities=['CAPABILITY_IAM',],
     			Tags=[
-    			{'Key': 'resource_owner','Value': 'andyr'},
+    			{'Key': 'resource_owner','Value': 'mchandira'},
     			{'Key': 'Purpose','Value': 'jira-testing'},
     			{'Key': 'business_unit','Value': 'FieldOps'},
     			{'Key':'Name','Value':stack},
--- delete_stack.py
+++ delete_stack.py
@@ -1,10 +1,8 @@
 #sample python code to delete cloudformation stacks
 import boto3
 
-
-stack_list = ['jiradc11-latest-1','jiradc22-latest-2','jiradc33-er-3','jiradc44-er-4','cnfdc11-latest-1','cnfdc22-latest-2','cnfdc33-er-3','cnfdc44-er-4','bbdc-1','bbdc-2']
-
-client = boto3.client('cloudformation',region_name='us-east-1')
+stack_list = ['jira-201', 'jira-202', 'jira-203', 'jira-204', 'jira-205', 'jira-206', 'jira-207', 'jira-208', 'jira-209', 'jira-210', 'jira-211', 'jira-212', 'jira-213', 'jira-214', 'jira-215', 'jira-216', 'jira-217', 'jira-218', 'jira-219', 'jira-220', 'jira-221', 'jira-222', 'jira-223', 'jira-224', 'jira-225', 'jira-226', 'jira-227', 'jira-228', 'jira-229', 'jira-230', 'jira-231', 'jira-232', 'jira-233', 'jira-234', 'jira-235', 'jira-236', 'jira-237', 'jira-238', 'jira-239', 'jira-240', 'jira-241', 'jira-242', 'jira-243', 'jira-244', 'jira-245', 'jira-246', 'jira-247', 'jira-248', 'jira-249', 'jira-250', 'jira-251', 'jira-252', 'jira-253', 'jira-254', 'jira-255', 'jira-256', 'jira-257', 'jira-258', 'jira-259', 'jira-260', 'jira-261', 'jira-262', 'jira-263', 'jira-264', 'jira-265', 'jira-266', 'jira-267', 'jira-268', 'jira-269', 'jira-270', 'jira-271', 'jira-272', 'jira-273', 'jira-274', 'jira-275', 'jira-276', 'jira-277', 'jira-278', 'jira-279', 'jira-280']
+client = boto3.client('cloudformation',region_name='us-west-2')
 
 for stack in stack_list:
 	response = client.delete_stack(
